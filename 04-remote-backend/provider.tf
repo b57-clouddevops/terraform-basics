960b9821412bc5ddf9a1a3af47f4b2941475fe9b
CodingManoj <CodingManoj@gmail.com>
2024-04-14 21:57:56 -0400

    Auto Push

--- 04-remote-backend/provider.tf
+++ 04-remote-backend/provider.tf
@@ -4,7 +4,7 @@ provider "aws" {}
 ## declaring the remote backend as s3
 terraform {
   backend "s3" {
-    bucket = "b57-tf-state-bucke"
+    bucket = "b57-tf-state-bucket"
     key    = "dev/ec2/terraform.tfstate"
     region = "us-east-1"
   }
