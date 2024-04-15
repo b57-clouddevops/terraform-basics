2e27091f61d8891e3b82f3ba1a043cd637324ca0
CodingManoj <CodingManoj@gmail.com>
2024-04-14 21:10:49 -0400

    Auto Push

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -1,14 +1,18 @@
+# Declaring the provider information
 provider "aws" {}
 
+# Defining the resources 
 resource "aws_instance" "web" {
   ami           = "ami-072983368f2a6eab5"
   instance_type = "t2.micro"
+
 
   tags = {
     Name = "tf-instance"
   }
 }
 
+# Printing the output
 output "private_ip_address" {
     value = aws_instance.web.private_ip
 }
