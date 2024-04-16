723b241f94c4b0610c362e2ff7b7140c5f4fefc5
CodingManoj <CodingManoj@gmail.com>
2024-04-15 21:27:23 -0400

    Auto Push

--- 07-functions/provider.tf
+++ 07-functions/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {}
 terraform {
   backend "s3" {
     bucket              = "b57-tf-state-bucket"
-    key                 = "dev/datasource/terraform.tfstate"
+    key                 = "dev/fun/terraform.tfstate"
     region              = "us-east-1"
   }
 }
