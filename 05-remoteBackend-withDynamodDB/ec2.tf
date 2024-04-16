16cd8afc9d527fa0ec1c45f0d7a2fd5f053ef141
CodingManoj <CodingManoj@gmail.com>
2024-04-15 21:09:21 -0400

    Auto Push

--- 05-remoteBackend-withDynamodDB/ec2.tf
+++ 05-remoteBackend-withDynamodDB/ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "app" {
   ami                       = "ami-072983368f2a6eab5"
   instance_type             = "t3.micro"
-  vpc_security_group_ids    = [aws_security_group.aws_security_group.id]
+  vpc_security_group_ids    = [aws_security_group.allow_tls.id]
 
 
   tags = {
