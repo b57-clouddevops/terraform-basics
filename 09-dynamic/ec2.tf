4617c026cec03edc215df7662755c65b5a71875e
CodingManoj <CodingManoj@gmail.com>
2024-04-16 22:46:33 -0400

    Auto Push

--- 09-dynamic/ec2.tf
+++ 09-dynamic/ec2.tf
@@ -15,18 +15,21 @@ output "private_ip" {
     value = aws_instance.app.private_ip
 }
 
-
 # Security Group 
 resource "aws_security_group" "allow_tls" {
   name        = "b57_allow_ssh"
   description = "Allow SSH inbound traffic and all outbound traffic"
 
-  ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
+  dynamic ingress {
+    for_each = var.ingress_rules
+        content {
+            description     = ingress.value["description"]
+            from_port       = ingress.value["from_port"]
+            to_port         = ingress.value["to_port"]
+            protocol        = ingress.value["protocol"]
+            cidr_blocks     = ingress.value["cidr_blocks"]
+            }
+        }
 
   egress {
     from_port       = 0
