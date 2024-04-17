ef0eaea1294bfea692891774a6205b1dd3d5ae16
CodingManoj <CodingManoj@gmail.com>
2024-04-16 22:50:16 -0400

    Auto Push

--- 09-dynamic/ec2.tf
+++ 09-dynamic/ec2.tf
@@ -42,3 +42,31 @@ resource "aws_security_group" "allow_tls" {
     Name = "allow_ssh_sg"
   }
 }
+
+
+resource "aws_security_group" "test_tls" {
+  name        = "b57_allow_ssh"
+  description = "Allow Test inbound traffic and all outbound traffic"
+
+  dynamic ingress {
+    for_each = var.ingress_rules
+        content {
+            description     = ingress.value["description"]
+            from_port       = ingress.value["from_port"]
+            to_port         = ingress.value["to_port"]
+            protocol        = ingress.value["protocol"]
+            cidr_blocks     = ingress.value["cidr_blocks"]
+            }
+        }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_sg"
+  }
+}
