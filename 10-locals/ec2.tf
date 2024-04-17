7f7a0f9f3ddd27579ab46fa2ca83b12b4017753f
CodingManoj <CodingManoj@gmail.com>
2024-04-16 23:45:00 -0400

    Auto Push

--- 10-locals/ec2.tf
+++ 10-locals/ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "app" {
   ami                       = "ami-072983368f2a6eab5"
   instance_type             = "t3.micro"
-  vpc_security_group_ids    = [aws_security_group.allow_tls.id]
+  vpc_security_group_ids    = [aws_security_group.allows_tls.id]
 
 
   tags = {
