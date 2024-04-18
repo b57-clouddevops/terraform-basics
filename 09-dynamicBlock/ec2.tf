61c13580751d09979d7a84d898af6df707b54935
CodingManoj <CodingManoj@gmail.com>
2024-04-17 20:47:58 -0400

    Auto Push

--- 09-dynamicBlock/ec2.tf
+++ 09-dynamicBlock/ec2.tf
@@ -1,5 +1,5 @@
 # Defining the resources 
-resource "aws_instance" "app" {
+resource "aws_instance" "apps" {
   ami                       = "ami-072983368f2a6eab5"
   instance_type             = "t3.micro"
   vpc_security_group_ids    = [aws_security_group.allow_tls.id]
@@ -12,34 +12,5 @@ resource "aws_instance" "app" {
 
 # Printing the output
 output "private_ip" {
-    value = aws_instance.app.private_ip
-}
-
-
-# Security Group 
-resource "aws_security_group" "allow_tls" {
-  name        = "b57_allow_ssh"
-  description = "Allow SSH inbound traffic and all outbound traffic"
-
-  dynamic ingress {
-    for_each = var.ingress_rules 
-    content {
-      description     = ingress.value["description"]
-      from_port       = ingress.value["from_port"]
-      to_port         = ingress.value["to_port"]
-      protocol        = ingress.value["protocol"]
-      cidr_blocks     = ingress.value["cidr_blocks"]
-    }
-  }
-  
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_ssh_sg"
-  }
+    value = aws_instance.apps.private_ip
 }
--- 09-dynamicBlock/provider.tf
+++ 09-dynamicBlock/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {}
 terraform {
   backend "s3" {
     bucket              = "b57-tf-state-bucket"
-    key                 = "dev/dynamodb/terraform.tfstate"
+    key                 = "dev/dynamic/terraform.tfstate"
     region              = "us-east-1"
     dynamodb_table      = "terraform-locking"
   }
